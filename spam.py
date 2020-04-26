4a3e3f7a859cc4464c88cdb237cfc5850507dee0
Yash Vij <37197637+yv3060@users.noreply.github.com>
2020-04-26 11:17:38 -0700

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,11 @@
+import pyautogui
+
+with open('pooja.txt','r') as file:
+    print(file.readline())
+
+'''
+for x in script.split(
+    pyautogui.write(x)
+    pyautogui.PAUSE = 0.5
+    pyautogui.press('enter')
+'''
